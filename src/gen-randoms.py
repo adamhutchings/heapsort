5c181c28c7406ad92c3697f0683ed3c90da53849
adamhutchings <adam.abahot@gmail.com>
2023-07-19 21:26:35 -0400

    Add logging for generation

--- src/gen-randoms.py
+++ src/gen-randoms.py
@@ -1,11 +1,28 @@
 import random
 import sys
+import time
 
 CT = int(sys.argv[1])
 
+print('Generating data ...')
+
+s = time.time()
+
 l = list(range(CT))
+
+m = time.time()
+print(f'Generated {CT} numbers in {round(m - s, 3)} seconds.')
+
 random.shuffle(l)
 
+e = time.time()
+print(f'Shuffled data in {round(e - m, 3)} seconds.')
+
+print('Done generating data.')
+print('Writing data ...')
+
 with open('data/random.txt', 'w') as file:
-    for x in l:
-        file.write(f'{x}\n')
+    for i in range(CT):
+        file.write(f'{l[i]}\n')
+        if i % (CT / 100) == 0:
+            print(f'Writing data: {int(i / (CT / 100))}% complete.')
